3a5471e561e530e62865fc52f633a0e6c64643a7
alplky <alicia.pelkey@gmail.com>
2020-11-15 11:15:44 -0600

    successfully obtained feature img url

--- Missions_to_Mars/mission_to_mars.py
+++ Missions_to_Mars/mission_to_mars.py
@@ -31,13 +31,14 @@ news_title = latest.text.strip()
 p_text = news_titles[0].find("div", class_="article_teaser_body")
 news_p = p_text.text.strip()
 
+print("NASA Mars News")
+print("-" * 30)
 print(news_title)
 print(news_p)
 
-
 # JPL Mars Space Images - Featured Image
 
-# establish url and scrape web page
+# establish url and go seach Mars images
 base_url = "https://www.jpl.nasa.gov"
 url = base_url + "/spaceimages/?search=&category=Mars"
 
@@ -48,6 +49,7 @@ driver.implicitly_wait(10)
 driver.find_element_by_link_text("FULL IMAGE").click()
 driver.find_element_by_partial_link_text("more info").click()
 
+# scrape page
 driver.implicitly_wait(10)
 html = driver.page_source
 
@@ -56,3 +58,15 @@ soup = BeautifulSoup(html, "html.parser")
 
 main_img = soup.find_all("img", class_="main_image")
 
+# extract out src attribute
+src = ""
+
+for image in main_img:
+    src = image["src"]
+
+# combine base url with src string and assign to variable
+featured_image_url = base_url + src
+
+print("Featured Mars Image URL")
+print("-" * 30)
+print(featured_image_url)
