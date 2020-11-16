2823fad60d2c32c853cc8a9bc009a02f0ec399ce
alplky <alicia.pelkey@gmail.com>
2020-11-16 14:19:38 -0600

    main route working, still working on rest

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,24 +1,32 @@
 # set up and dependencies
 from scrape_mars import scrape
 import os
-from Flask import Flask, render_template
+from flask import Flask, render_template
 import pymongo
 
 # set up connections to Mongo Database
-# CONN = os.getenv("CONN")
-# client = pymongo.MongoClient(CONN)
-# db = client.mars
-# db.mars.drop()
+CONN = os.getenv("CONN")
+client = pymongo.MongoClient(CONN)
+db = client.mars
+
+db.mars.drop()
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    for result in db.mars.find():
-        results.append({key: value for key, value in result.items()})
-    return render_template("index.html", )
+    return "The app is up!"
+
+
+#     for result in db.mars.find():
+#         results.append({key: value for key, value in result.items()})
+#     return render_template("index.html", mars_dict=results)
 
 @app.route("/scrape")
 def scrape_route():
-    db.mars.insert_many(scrape(driver))
-    return 
+    db.mars.insert_many(scrape())
+    return "Successfully scraped webpages!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
