c0e04b30a6952b6f0d5be2e1607ceebd3f3413c6
alplky <alicia.pelkey@gmail.com>
2020-11-16 12:20:59 -0600

    still working, haven't tested yet

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,21 +1,24 @@
 # set up and dependencies
 from scrape_mars import scrape
 import os
-from Flask import Flask, jsonify
+from Flask import Flask, render_template
 import pymongo
 
 # set up connections to Mongo Database
 # CONN = os.getenv("CONN")
 # client = pymongo.MongoClient(CONN)
 # db = client.mars
-# db.students.drop()
+# db.mars.drop()
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return 
+    for result in db.mars.find():
+        results.append({key: value for key, value in result.items()})
+    return render_template("index.html", )
 
 @app.route("/scrape")
 def scrape_route():
     db.mars.insert_many(scrape(driver))
+    return 
